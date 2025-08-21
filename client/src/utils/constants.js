72da1e24c53cb71f59001dce331c2645d1b8bdc5
TanuPayasi <tanupayasi002@gmail.com>
2025-08-21 23:13:49 +0530

    Create constants.js

---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const difficulties = ['Easy','Medium','Hard'];
+export const statuses = ['Not Started','In Progress','Solved','Skipped'];
+export const topics = ['Arrays','Strings','Linked List','Trees','Graphs','DP','Greedy','Math','Two Pointers','General'];
